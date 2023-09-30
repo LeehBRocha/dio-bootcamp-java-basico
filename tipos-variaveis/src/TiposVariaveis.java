31ee724962ef0078e7e6545081125be73c477be8
LeehBRocha <batista.rocha@ufms.br>
2023-09-30 19:33:40 -0400

    segundo commit

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,13 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int idade = 15;
+        double pi = 3.14;
+        final double peso = 52.5;
+        char sexo = 'M';
+        System.out.println(idade);
+        System.out.println(peso);
+        System.out.println(pi);
+
+
     }
 }
